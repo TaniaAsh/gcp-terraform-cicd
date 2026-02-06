fe9170981286599436cde9ed0ab787160f3695c3
Tania Ash <tania.ash@cloudreach.com>
2026-02-06 20:42:30 +0000

    init terraform gcp bucket

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "projrancher"
+  region  = "europe-west1"
+}
+
+resource "google_storage_bucket" "demo" {
+  name                        = "tf-local-demo-taniaash12345"
+  location                    = "EU"
+  uniform_bucket_level_access = true
+}
